4134a50ac8b8d7fa3998dd2255f5b599c1272240
221029cs06 <113498631+221029cs06@users.noreply.github.com>
2023-01-31 14:43:19 +0500

    Add files via upload

---
+++ homework_1.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  homework_1
+//
+//  Created by Asilbek Alimov on 31/01/23.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World";
+    return 0;
+}
